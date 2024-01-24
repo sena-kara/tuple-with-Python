c8e70223bc65f0e2348fac57b71b0a0e3186c3c1
Senakara <106998274+sena-kara@users.noreply.github.com>
2024-01-24 21:53:26 +0300

    Add files via upload
    tuple with Python

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+tupleListe = (2,4,6,"Ankara")
+liste = [2,4,6,"Ankara"]
+print(type(tupleListe))
+print(type(liste))
+print(tupleListe)
+print(liste)
+
+tupleListe2 = (2,4,6,"Ankara",(2,3,4,))
+print(len(tupleListe2))
+
